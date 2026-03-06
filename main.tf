b0c3e631f0662b6c0f4d142567d1e39e66a8fe5e
AjitPunchhiInutive <ajit.punchhi@intuitve.cloud>
2026-03-07 01:04:59 +0530

    adding file

--- main.tf
+++ main.tf
@@ -2,9 +2,28 @@ locals {
 
   project_config_files = fileset("config/project-factory", "*.yaml")
   project_objects = [for f in local.project_config_files : yamldecode(file("config/project-factory/${f}"))]
+
+  
+  secret_config_files = fileset("config/secretmanager", "*.yaml")
+  secrets     = { for f in local.secret_config_files : trimsuffix(f, ".yaml") => yamldecode(file("config/secretmanager/${f}"))["secrets"] }
+
+# sa_config_files = fileset("config/serviceaccount", "*.yaml")
+  # sa_objects      = [for f in local.sa_config_files : yamldecode(file("config/serviceaccount/${f}"))]
 }
 
 module "project-factory" {
   source          = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//project-factory?ref=main"
   project_objects = local.project_objects
+}
+
+# module "serviceaccount" {
+#   source          = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//serviceaccount?ref=main"
+#   project_objects = local.project_objects
+#   sa_objects      = local.sa_objects
+# }
+
+module "secret-manager" {
+  source   = "git@github.com:AjitPunchhiInutive/-sw-prod-udp-rds-infra-modules.git//secretmanager?ref=main"
+  for_each = local.secrets
+  secrets  = each.value
 }
